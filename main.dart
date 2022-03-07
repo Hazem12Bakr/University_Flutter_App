2abe179edfa90ff6cfd0d0058203d91dc72d238d
Abdallah1Habib <94358402+Abdallah1Habib@users.noreply.github.com>
2022-03-07 13:57:46 +0200

    create main.dart

---
+++ main.dart
@@ -0,0 +1,11 @@
+/*
+the main dart file for our project 
+still empty until we finish learning about flutter 
+Team members:
+1- Abdallah Mohamed Habib
+2- Ahmed Sayed Abodeif
+3- Hazem Abobaker
+4- Mohamed Abdelaleim
+5- Mohamed Gaber
+6- Ziad Zakaria
+*/
